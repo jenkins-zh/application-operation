855dbbb4d0edf3742ccfe160705b3eacf6700917
anxk <anxiaokang@hotmail.com>
2020-04-12 02:32:30 +0800

    Modify: 去掉apps/ssh-key-killer/.idea，修改 pub_key 配置方式
    Signed-off-by: anxk <anxiaokang@hotmail.com>

--- apps/ssh-key-killer/authorized_keys.py
+++ apps/ssh-key-killer/authorized_keys.py
@@ -11,12 +11,11 @@ class AuthorizedKeys:
         with open(self.filename) as f:
             temp = f.readlines()
         for line in temp:
-            if line.strip(" \n") != "":
-                self.data[line.strip(" \n").split(" ")[-1]] = line
+            self.data[line] = None
 
-    def invalid(self, host):
-        self.data[host] = "x" + self.data.get(host)
+    def invalid(self, key):
+        self.data.pop(key)
 
     def save(self):
         with open(self.filename, "w+") as f:
-            f.writelines(line for line in self.data.values())
+            f.writelines(line for line in self.data.keys())
--- apps/ssh-key-killer/killer.py
+++ apps/ssh-key-killer/killer.py
@@ -11,19 +11,19 @@ class Killer:
         self.config = load_config(configfile)
 
     def get_invalid_hosts(self):
-        invalid_hosts = []
+        invalid_pub_key = []
         for host in self.config.get("hosts"):
             if datetime.strptime(str(host.get("expire_time")), "%Y%m%d") < datetime.now():
-                invalid_hosts.append(host.get("host"))
-        return invalid_hosts
+                invalid_pub_key.append(host.get("pub_key"))
+        return invalid_pub_key
 
     def invalid(self):
         authorized_keys_files = scan_authorized_keys()
-        invalid_hosts = self.get_invalid_hosts()
+        invalid_pub_key = self.get_invalid_hosts()
         for file in authorized_keys_files:
             ak = AuthorizedKeys(file)
-            for host in ak.data.keys():
-                if host in invalid_hosts:
-                    ak.invalid(host)
-                    logger.info("invalid host {}".format(host))
+            for pub_key in ak.data.keys():
+                if pub_key in invalid_pub_key:
+                    ak.invalid(pub_key)
+                    logger.info("invalid host {}".format(pub_key))
             ak.save()
